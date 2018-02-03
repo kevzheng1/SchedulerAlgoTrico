e7db6c243a0203a273050248cc824d1913d57347
kevzheng1 <35941399+kevzheng1@users.noreply.github.com>
2018-02-03 10:12:57 -1000

    error handling

--- main.py
+++ main.py
@@ -9,5 +9,7 @@ totalCourses = int(input())
 for i in range(totalCourses):
     print ("Time for this course: ex. 11:20AM-12:35PM")
     courseRequests.append(Course("Test", "M", input(), "Swarthmore"))
-    secondTestCal.addCourse(courseRequests[i].getStart_time(), courseRequests[i].getEnd_time())
-    print(secondTestCal.returnCal("blocks"))
+    if secondTestCal.addCourse(courseRequests[i].getStart_time(), courseRequests[i].getEnd_time()):
+        print(secondTestCal.returnCal("blocks"))
+    else:
+        print("Conflict Detected!")
