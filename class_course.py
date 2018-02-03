4df681b4523bd4278b3441ad3c7392c20578e4fa
peemlerd <peemlerd@gmail.com>
2018-02-03 16:04:57 -0500

    modify the weekday part

--- class_course.py
+++ class_course.py
@@ -1,4 +1,5 @@
 from time_parser import *
+from day_parser import *
 
 class Course:
     # Input: name (str), weekday (str), start to end time (str) e.g. "11:20AM-12:35PM"
@@ -6,12 +7,38 @@ class Course:
     def __init__(self, name, weekday, duration, location):
         self.name       = name
         self.weekday    = weekday
-        class_period = time_parser(duration)
-        self.start_time = class_period[0]
-        self.end_time   = class_period[1]
+        self.duration   = duration
         self.location   = location
-        # Start converting these weekdays str into list of start time and end time
-        # Let's say we have MWF as our weekday input
+        # This part fixes the function.
+        class_weekday   = weekday_parser(weekday)
+        class_duration  = time_parser(duration)
+        start_time      = class_duration[0]
+        end_time        = class_duration[1]
+        class_start_lst = []
+        class_end_lst   = []
+        five_min_num    = 288 # number of five-minute block every day
+        n = len(class_weekday)
+        for i in range(n):
+            if class_weekday == "M":
+                class_start_lst.append(start_time)
+                class_end_lst.append(end_time)
+            elif class_weekday == "T":
+                class_start_lst.append(start_time + five_min_num)
+                class_end_lst.append(end_time + five_min_num)
+            elif class_weekday == "W":
+                class_start_lst.append(start_time + 2 * five_min_num)
+                class_end_lst.append(end_time + 2 * five_min_num)
+            elif class_weekday == "H":
+                class_start_lst.append(start_time + 3 * five_min_num)
+                class_end_lst.append(end_time + 3 * five_min_num)
+            elif class_weekday == "F":
+                class_start_lst.append(start_time + 4 * five_min_num )
+                class_end_lst.append(end_time + 4 * five_min_num)
+        # Now, the self part continues.
+        self.start_time = class_start_lst
+        self.end_time   = class_end_lst
+        # Use self.start_time and self.end_time to change the boolean value
+        # on the calendar
 
     def getName(self):
         return self.name
@@ -19,17 +46,19 @@ class Course:
     def getWeekday(self):
         return self.weekday
 
+    def getDuration(self):
+        return self.duration
+
+    def getLocation(self):
+        return self.location
+
     def getStart_time(self):
         return self.start_time
 
     def getEnd_time(self):
         return self.end_time
 
-    def getLocation(self):
-        return self.location
-
     def __str__(self):
         result = "Course name: %s" %(self.name) + "\n" + self.weekday + "\n"
-        result += str(self.start_time) + "\n" + str(self.end_time) + "\n"
-        result += self.location + "\n"
+        result += str(self.duration) + "\n" + self.location + "\n"
         return result
