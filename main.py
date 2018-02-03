3f27bc934153bfbb8258ca37752c7c91f1490da0
kevzheng1 <35941399+kevzheng1@users.noreply.github.com>
2018-02-03 11:14:04 -1000

    fine

--- main.py
+++ main.py
@@ -1,14 +1,19 @@
 from calendarclass import *
 from class_course import *
 from time_parser import *
+from day_parser import *
+import json
+
+Spring2018 = json.load(open("Spring_2018.json"))
+print(Spring2018[0])
 
 courseRequests = []
 secondTestCal = Calendar()
 print ("How many courses do you want to add?")
 totalCourses = int(input())
 for i in range(totalCourses):
-    print ("Time for this course: ex. 11:20AM-12:35PM")
-    courseRequests.append(Course("Test", "M", input(), "Swarthmore"))
+    print ("Time for this course: and also day ex. 11:20AM-12:35PM")
+    courseRequests.append(Course("Test", input(), input(), "Swarthmore"))
     if secondTestCal.addCourse(courseRequests[i].getStart_time(), courseRequests[i].getEnd_time()):
         print(secondTestCal.returnCal("blocks"))
     else:
