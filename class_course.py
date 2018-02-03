3a2e8a1836146719ad95b420b7fbd0e1ed51c5fe
peemlerd <peemlerd@gmail.com>
2018-02-03 12:54:09 -0500

    Class Course

---
+++ class_course.py
@@ -0,0 +1,35 @@
+class Course:
+    # Input: name (str), weekday (str), start_time (int)
+    # end_time (int), location (str)
+    def __init__(self, name, weekday, start_time, end_time, location):
+        self.name       = name
+        self.weekday    = weekday
+        self.start_time = start_time
+        self.end_time   = end_time
+        self.location   = location
+        # Start converting these weekdays str into list of start time and end time
+        # Let's say we have MWF as our weekday input
+        weekday_input "MWF"
+
+        self.total_time = 0 # will be fixed later
+
+    def getName(self):
+        return self.name
+
+    def getWeekday(self):
+        return self.weekday
+
+    def getStart_time(self):
+        return self.start_time
+
+    def getEnd_time(self):
+        return self.end_time
+
+    def getLocation(self):
+        return self.location
+
+    def __str__(self):
+        result = "Course name: %s" %(self.name) + "\n" + self.weekday + "\n"
+        result += str(self.start_time) + "\n" + str(self.end_time) + "\n"
+        result += self.location + "\n"
+        return result
